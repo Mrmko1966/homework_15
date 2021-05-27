e232579a8e1c303d64c5e656487c7520d78f008b
Mrmko1966 <83647563+Mrmko1966@users.noreply.github.com>
2021-05-27 08:04:18 +0400

    aaaaad

--- homework_21.py
+++ homework_21.py
@@ -6,41 +6,24 @@ class Actor(object):
         self.surname = surname
         self.url = url
 
-def to_json(self):
-    data_ = {
-        'name': self.name,
-        'surname': self.surname,
-        'age': self.url
-    }
-    return data_
+    def to_json(self):
+        data_ = {
+            'name': self.name,
+            'surname': self.surname,
+            'age': self.url
+        }
+        return data_
+    def __repr__(self):
+        return F"{self.name}-{self.surname} {self.url}"
+
+with open("sample_json.json", "r") as json_file:
+    data = json.load(json_file)
+
+for item in data["items"]:
+    response = requests.get(item["url"])
+    a= "{} {}".format(item["name"],item["surname"])
+
+    with open(F"{a}.jpeg", 'wb') as file:
+        file.write(response.content)
 
 
-dict1 = {
-
-
-
-
-        "name": "Natalia Oreiro",
-        "url_":"http://t2.gstatic.com/licensed-image?q=tbn:ANd9GcT3_8PjjUpfY8hTiGR0xqp44kD16ksLOzJAn0GNjSkxi4S1RWQCuq_wCACvjDvz"
-    }
-dict2 = {
-        "name": "jessica simpson",
-        "url_":"https://upload.wikimedia.org/wikipedia/commons/f/f2/Jessica_Simpson_Joining_Forces_with_the_Rockies_April_2011_%28cropped%29.jpg"
-    }
-dict3 = {
-        "name": "Adel",
-        "url_":"https://uk.toluna.com//dpolls_images/2016/04/23/5bc72609-421c-4067-b66e-135d028b76b8.jpg"
-    }
-json_dump = json.dumps(dict1,dict3,dict2)
-print(json_dump)
-# with open("sample_json.json", 'r') as json_in
-
-    # response = requests.get(json_dump[dict_][name])
-    # print(response)
-    # with open(name, 'wb') as file:
-    #     file.write(response.content)
-
-
-# print(response.text)
-#
-# print(response.json())
